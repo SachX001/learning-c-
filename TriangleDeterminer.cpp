bab0a75d02f59254c7e8a278575732ff27c4b45b
SachX001 <anubhavsachan72@gmail.com>
2025-09-09 20:43:25 +0530

    Create TriangleDeterminer.cpp

---
+++ TriangleDeterminer.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // update your code here
+    float side1, side2, side3;
+    cin >> side1 >> side2 >> side3;
+    
+    if(side1 == side2 && side2 == side3) {
+        cout << "equilateral" << endl;
+    }
+    else if(side1 == side2 || side2 == side1 || side1 == side3) {
+        cout << "isosceles" << endl;
+    }
+    else {
+        cout << "scalene" << endl;
+    }
+
+    return 0;
+}
